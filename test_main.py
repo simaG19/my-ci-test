2a67d30dc2a1d7934f86b32feaf02b731ede5f30
simon <natibin033@gmail.com>
2025-02-25 08:55:59 +0300

    test5

--- test_main.py
+++ test_main.py
@@ -3,7 +3,7 @@ from main import add
 
 class TestMath(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(2, 3), 6)
+        self.assertEqual(add(2, 3), 10)
 
 if __name__ == "__main__":
     unittest.main()
